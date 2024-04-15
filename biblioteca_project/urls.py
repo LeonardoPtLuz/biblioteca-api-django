36e0ea1f319f585457d1dc7930241c5c28059e20
java_ada <leonardoptluz@hotmail.com>
2024-04-15 03:30:14 -0300

    v1

--- biblioteca_project/urls.py
+++ biblioteca_project/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from livros_app.api import viewsets as livrosviewsets
+from django.conf.urls.static import static
+from django.conf import settings
 
 route = routers.DefaultRouter()
 
@@ -11,4 +13,4 @@ route.register(r'livros', livrosviewsets.LivrosViewSet, basename="Livros")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- livros_app/models.py
+++ livros_app/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from uuid import uuid4
 
 
+
+def up_imagem_livro(instace, filename):
+    return f"{instace.id_livro}-{filename}"
+
 class Livros(models.Model):
     id_livro = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     titulo = models.CharField(max_length=255)
@@ -11,3 +15,4 @@ class Livros(models.Model):
     paginas = models.IntegerField()
     editora = models.CharField(max_length=255)
     criado_em = models.DateField(auto_now_add=True)
+    imagem = models.ImageField(upload_to=up_imagem_livro, blank=True, null=True)
